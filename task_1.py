a73a7026f21fbd2532432b110660bb2a419f198f
DamnFilthy <aleksandr-shiryaev-1993@mail.ru>
2020-10-02 10:27:43 +0300

    initial commit

---
+++ task_1.py
@@ -0,0 +1,58 @@
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+def get_recipes(filename):
+    result = {}
+    with open(filename, encoding='utf-8') as recipes:
+        line = recipes.readline()
+        recipe_name = None
+        ingred_count = -1
+        while line != '':
+            line = line.strip()
+            if recipe_name is None:
+                recipe_name = line
+                result[recipe_name] = []
+                line = recipes.readline()
+                continue
+            if ingred_count < 0:
+                try:
+                    ingred_count = int(line)
+                except ValueError as e:
+                    return {}
+            elif ingred_count == 0:
+                recipe_name = None
+                ingred_count = -1
+            else:
+                tmp_dict = dict(zip(
+                    ['ingredient_name', 'quantity', 'measure'],
+                    [x.strip() for x in line.split('|')]))
+                try:
+                    tmp_dict['quantity'] = int(tmp_dict.setdefault('quantity', 0))
+                except ValueError as e:
+                    return {}
+                result[recipe_name].append(tmp_dict)
+                ingred_count -= 1
+            line = recipes.readline()
+    return result
+
+def get_shop_list_by_dishes(dishes, person_count, filename='recipes.txt'):
+    result = {}
+    for dish in dishes:
+        ingred_list = get_recipes(filename)[dish]
+        for item in ingred_list:
+            ingred_name = item['ingredient_name']
+            ingred_found = result.setdefault(ingred_name,
+                                             {'measure': item['measure'],
+                                             'quantity': 0})
+            ingred_found['quantity'] = ingred_found['quantity'] + \
+            item['quantity']
+            result[ingred_name] = ingred_found
+    return result
+
+print('Рецепты:')
+cook_book = get_recipes('recipes.txt')
+pp.pprint(cook_book)
+
+print('\nНеобходимые покупки для блюд:')
+pp.pprint(get_shop_list_by_dishes(['Запеченный картофель', \
+'Утка по-пекински'], 3))
